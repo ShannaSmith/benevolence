3b03a32234d4dd5678afa8dc1f3e58a28ff0fb5a
ShannaSmith <shannagjh@gmail.com>
2022-12-06 14:20:00 -0500

    make changes to model.py from code review & create homepage skeleton

--- model.py
+++ model.py
@@ -9,12 +9,9 @@ class User(db.Model):
     user_id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
     fname = db.Column(db.String(25), nullable=False)
     lname = db.Column(db.String(30), nullable=True)
-    email = db.Column(db.VARCHAR(70), nullable=False, unique=True)
-    recipient_id = db.Column(db.Integer, db.ForeignKey("recipients.recipient_id"), nullable=False)
-    calendar_id = db.Column(db.Interger, db.ForeignKey("calendars.calendar_id"), nullable=False)
-    recipient = db.relationship('Recipient', back_populates='user')
-    calendar = db.relationship('Calendar', back_populates='user')
-
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    recipients = db.relationship('Recipient', back_populates='user')
+   
     def __repr__(self):
         return f"<User ID={self.user_id} Name={self.fname} {self.lname} Email={self.email}>"
 
@@ -23,15 +20,12 @@ class Recipient(db.Model):
     __tablename__ = "recipients"
     recipient_id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
-    # ranking = db.Column(db.Integer, nullable=True)
-    event_id = db.Column(db.Integer, db.ForeignKey("events.event_id"), nullable=False)
-    note_id = db.Column(db.Integer, db.ForeignKey("notes.note_id"), nullable=False)
+    ranking = db.Column(db.Integer, nullable=True)
     user_id = db.Column(db.Integer, db.ForiegnKey("users.user_id"), nullable=False)
-    like_id = db.Column(db.Integer, db.ForiegnKey("likes.like_id"))
-    event = db.relationship('Event', back_populates='recipient')
-    note = db.relationship('Note', back_populates='recipient')
-    user = db.relationship('User', back_populates='recipient')
-    like = db.relationship('Like', back_populates='recipient')
+    events = db.relationship('Event', back_populates='recipient')
+    notes = db.relationship('Note', back_populates='recipient')
+    user = db.relationship('User', back_populates='recipients')
+    likes = db.relationship('Like', back_populates='recipient')
 
     def __repr__(self):
         return f"<Recipient ID={self.recipient_id} Name={self.name}>"
@@ -43,7 +37,7 @@ class Like(db.Model):
     like_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     like_name = db.Column(db.String(40), nullable=False)
     recipient_id = db.Column(db.Integer, db.ForeignKey("recipients.recipient_id"), nullable=False)
-    recipient = db.relationship('Recipient', back_populates='like')
+    recipient = db.relationship('Recipient', back_populates='likes')
 
     def __repr__(self):
         return f"<Like ID={self.like_id} Name={self.like_name}>"
@@ -56,8 +50,11 @@ class Event(db.Model):
     event_date = db.Column(db.Date, nullable=False)
     user_id = db.Column(db.Integer, db.ForiegnKey("users.user_id"))
     recipient_id = db.Column(db.Integer, db.ForiegnKey("recipients.recipient_id"))
+    note_id = db.Column(db.Integer, db.ForiegnKey("notes.note_id"), nullable=True)
+
     user = db.relationship('User', back_populates='event')
-    recipient = db.relationship('Recipient', back_populates='event')
+    recipient = db.relationship('Recipient', back_populates='events')
+    note = db.relationship('Note', back_populates='event')
 
     def __repr__(self):
         return f"<Event ID={self.event_id} Name={self.event_name} Date={self.event_date}>"
@@ -70,11 +67,12 @@ class Note(db.Model):
     content = db.Column(db.Text, nullable=True)
     recipient_id = db.Column(db.Integer, db.ForiegnKey("recipients.recipient_id"), nullable=False)
     event_id = db.Column(db.Integer, db.ForiegnKey("events.event_id"), nullable=True)
-    recipient = db.relationship('Recipient', back_populates='note')
+
+    recipient = db.relationship('Recipient', back_populates='notes')
     event = db.relationship('Event', back_populates='note')
 
     def __repr__(self):
-        return f"<Note ID={self.note_id} Content={self.content}>"
+        return f"<Note ID={self.note_id} Content={self.content[:20]}>"
 
 
 
